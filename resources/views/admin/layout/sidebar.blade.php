82a04e6e740cc5253f59e722078ca6e110fecdb3
dodaydream <daydream2013@live.com>
2020-01-10 10:06:50 +0800

    feat: modify sidebar

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,11 +1,12 @@
 <div class="sidebar">
     <nav class="sidebar-nav">
         <ul class="nav">
-            <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.content') }}</li>
+            <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.stock-management') }}</li>
             <li class="nav-item"><a class="nav-link" href="{{ url('admin/products') }}"><i class="nav-icon icon-drop"></i> {{ trans('admin.product.title') }}</a></li>
+           <li class="nav-item"><a class="nav-link" href="{{ url('admin/inventories') }}"><i class="nav-icon icon-compass"></i> {{ trans('admin.inventory.title') }}</a></li>
+            <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.category') }}</li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/categories') }}"><i class="nav-icon icon-energy"></i> {{ trans('admin.category.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/brands') }}"><i class="nav-icon icon-star"></i> {{ trans('admin.brand.title') }}</a></li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/inventories') }}"><i class="nav-icon icon-compass"></i> {{ trans('admin.inventory.title') }}</a></li>
            {{-- Do not delete me :) I'm used for auto-generation menu items --}}
 
             <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.settings') }}</li>
